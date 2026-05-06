fa8363db6d8cee4185154221f02632819691712f
RoberMaher <robermaher@gmail.com>
2026-05-06 19:13:05 +0300

    Add Task4 Session3

--- Tasks/Session3/main.dart
+++ Tasks/Session3/main.dart
@@ -1,20 +1,25 @@
 import 'dart:io';
 
 // import 'Car.dart';
-import 'BankAccount.dart';
+// import 'BankAccount.dart';
+import 'Rectangle.dart';
 
 void main(){
   // Car(model: "KIA", year: 2011);
   
-  BankAccount account = BankAccount();
+  // BankAccount account = BankAccount();
 
-  print("Enter amount to deposit:");
-  num amountForDeposit = num.parse(stdin.readLineSync()!);
-  account.deposit(amountForDeposit);
+  // print("Enter amount to deposit:");
+  // num amountForDeposit = num.parse(stdin.readLineSync()!);
+  // account.deposit(amountForDeposit);
 
 
-  print("Enter amount to withdrow:");
-  num amountForWithdrow = num.parse(stdin.readLineSync()!);
-  account.withdrow(amountForWithdrow);
+  // print("Enter amount to withdrow:");
+  // num amountForWithdrow = num.parse(stdin.readLineSync()!);
+  // account.withdrow(amountForWithdrow);
+
+  Rectangle rectangle = Rectangle(lengh: 5, width: 10);
+   num area = rectangle.calculateArea();
+   print("The Area of Rectangle is: $area");
 
 }
