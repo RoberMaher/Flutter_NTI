20955c0272f913ab67468afb8ab63e6df3c815d5
RoberMaher <robermaher@gmail.com>
2026-05-09 04:42:38 +0300

    Add Task3 Session3

--- Tasks/Session3/Book.dart
+++ Tasks/Session3/Book.dart
@@ -1,3 +1,8 @@
+// Exercise 3: Book Class
+// Create a Book class with properties for title, author, and publicationYear. Implement a 
+// method called printInfo that prints the book's information. Create two Book objects and 
+// call the printInfo method for each.
+
 class Book {
   String title;
   String author;
