e46691a5dc68fcecf542de7c4fb261af80bf7b33
RoberMaher <robermaher@gmail.com>
2026-05-19 23:03:01 +0300

    Update login Screen

--- Tasks/Session4/login_screen/lib/home.dart
+++ Tasks/Session4/login_screen/lib/home.dart
@@ -23,67 +23,72 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(color: const Color.fromARGB(255, 18, 79, 94)),
-
-          Column(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            alignment: Alignment.center,
             children: [
-              const Spacer(flex: 2),
-
-              const PageTitle(text: "MOMENTUM"),
-
-              const Titles(
-                title1: "GROWTH",
-                title2: "HAPPENS",
-                title3: "TODAY",
+              Container(color: const Color.fromARGB(255, 18, 79, 94)),
+          
+              Column(
+                children: [
+                  const Spacer(flex: 2),
+          
+                  const PageTitle(text: "MOMENTUM"),
+          
+                  const Titles(
+                    title1: "GROWTH",
+                    title2: "HAPPENS",
+                    title3: "TODAY",
+                  ),
+          
+                  const Spacer(flex: 1),
+          
+                  const Item(text1: "SIGN IN", text2: "SIGN UP"),
+          
+                  const SizedBox(height: 50),
+          
+                  CustomFormField(
+                    text: "Enter your Email",
+                    icon: Icons.email,
+                    controller: emailController,
+                    isPassword: false,
+                    formKey: emailFormKey,
+                  ),
+          
+                  const SizedBox(height: 30),
+          
+                  CustomFormField(
+                    text: "Enter your Password",
+                    icon: Icons.lock,
+                    controller: passwordController,
+                    isPassword: true,
+                    formKey: passwordFormKey,
+                  ),
+          
+                  const SizedBox(height: 50),
+          
+                  Button(
+                    text: "SIGN IN",
+                    onTap: () {
+                      final emailValid = emailFormKey.currentState!.validate();
+          
+                      final passwordValid = passwordFormKey.currentState!
+                          .validate();
+          
+                      if (emailValid && passwordValid) {
+                        Navigator.pushReplacementNamed(context, '/XoGame');
+                      }
+                    },
+                  ),
+          
+                  const Spacer(flex: 2),
+                ],
               ),
-
-              const Spacer(flex: 1),
-
-              const Item(text1: "SIGN IN", text2: "SIGN UP"),
-
-              const SizedBox(height: 50),
-
-              CustomFormField(
-                text: "Enter your Email",
-                icon: Icons.email,
-                controller: emailController,
-                isPassword: false,
-                formKey: emailFormKey,
-              ),
-
-              const SizedBox(height: 30),
-
-              CustomFormField(
-                text: "Enter your Password",
-                icon: Icons.lock,
-                controller: passwordController,
-                isPassword: true,
-                formKey: passwordFormKey,
-              ),
-
-              const SizedBox(height: 50),
-
-              Button(
-                text: "SIGN IN",
-                onTap: () {
-                  final emailValid = emailFormKey.currentState!.validate();
-
-                  final passwordValid = passwordFormKey.currentState!
-                      .validate();
-
-                  if (emailValid && passwordValid) {
-                    Navigator.pushReplacementNamed(context, '/XoGame');
-                  }
-                },
-              ),
-
-              const Spacer(flex: 2),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
