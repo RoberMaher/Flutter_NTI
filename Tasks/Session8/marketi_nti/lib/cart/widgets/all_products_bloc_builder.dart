4e4d958513fd35442f9e0205e2c9db6df4c6155b
Rober Maher <robermaher@gmail.com>
2026-06-06 19:06:40 +0300

    Adds AllProductsBlocBuilder widget for improved product state management in the cart

---
+++ Tasks/Session8/marketi_nti/lib/cart/widgets/all_products_bloc_builder.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:marketi_nti/cart/cubit/products_cubit.dart';
+import 'package:marketi_nti/cart/widgets/product_card.dart';
+
+class AllProductsBlocBuilder extends StatelessWidget {
+  const AllProductsBlocBuilder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProductsCubit, ProductsState>(
+      builder: (context, state) {
+        if (state is ProductsLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is ProductsSuccess) {
+          return GridView.builder(
+            itemCount: state.products.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.8,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              return ProductCard(
+                index: index,
+                productModel: state.products[index],
+              );
+            },
+          );
+        } else if (state is ProductsFailure) {
+          return Center(child: Text(state.errorMessage));
+        } else {
+          return const SizedBox();
+        }
+      },
+    );
+  }
+}
