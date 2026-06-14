d5a9cc21c6b3959e0953976e640dae120a4b0a32
Rober Maher <robermaher@gmail.com>
2026-06-14 18:29:56 +0300

    Refactor handleError function to improve error handling and remove UI dependencies

--- Tasks/Session8/marketi_nti/lib/core/networking/handle_error.dart
+++ Tasks/Session8/marketi_nti/lib/core/networking/handle_error.dart
@@ -1,44 +1,68 @@
-import 'package:awesome_dialog/awesome_dialog.dart';
+// ignore: unused_element
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
+import 'package:marketi_nti/auth/models/error_model.dart';
 
-HandleError(DioException e, BuildContext context) {
-  switch (e.response!.statusCode) {
-    case 400:
-      String errorMessage = '';
+String handleError(
+  DioException e,
+) {
+  if (e.type case DioExceptionType.badResponse) {
+    switch (e.response?.statusCode) {
+      case 400:
+        ErrorModel errorModel = ErrorModel.fromJson(e.response!.data);
+        String errorMessage = '';
+        errorModel.errors.forEach((error) => errorMessage += '$error\n');
 
-      final errors = e.response!.data['errors'];
+        return errorMessage;
 
-      if (errors is Map) {
-        errors.forEach((key, value) {
-          for (var err in value) {
-            errorMessage += '$err\n';
-          }
-        });
-      } else if (errors is List) {
-        for (var err in errors) {
-          errorMessage += '$err\n';
-        }
-      }
+      case 401:
+        ErrorModel errorModel = ErrorModel.fromJson(e.response!.data);
+        String errorMessage = '';
+        errorModel.errors.forEach((error) => errorMessage += '$error\n');
 
-      AwesomeDialog(
-        context: context,
-        dialogType: DialogType.error,
-        animType: AnimType.scale,
-        title: '${e.response!.data['message']}',
-        desc: errorMessage.trim(),
-        btnOkOnPress: () {},
-      ).show();
-      break;
+          return errorMessage;
 
-    default:
-      AwesomeDialog(
-        context: context,
-        dialogType: DialogType.error,
-        animType: AnimType.scale,
-        title: 'Error',
-        desc: 'Something went wrong. Please try again later.',
-        btnOkOnPress: () {},
-      ).show();
+      case 403:
+        ErrorModel errorModel = ErrorModel.fromJson(e.response!.data);
+        String errorMessage = '';
+        errorModel.errors.forEach((error) => errorMessage += '$error\n');
+
+        return errorMessage;
+
+      case 404:
+        ErrorModel errorModel = ErrorModel.fromJson(e.response!.data);
+        String errorMessage = '';
+        errorModel.errors.forEach((error) => errorMessage += '$error\n');
+
+        return errorMessage;
+
+      case 422:
+        ErrorModel errorModel = ErrorModel.fromJson(e.response!.data);
+        String errorMessage = '';
+        errorModel.errors.forEach((error) => errorMessage += '$error\n');
+
+        return errorMessage;
+
+      case 500:
+        log('Internal Server Error');
+        return 'Internal Server Error. Please try again later.';
+      //
+    }
+  } else if (e.type case DioExceptionType.connectionTimeout) {
+    return 'Connection timed out. Please try again later.';
+  } else if (e.type case DioExceptionType.receiveTimeout) {
+    return 'Connection timed out. Please try again later.';
+  } else if (e.type case DioExceptionType.sendTimeout) {
+    return 'Connection timed out. Please try again later.';
+  } else if (e.type case DioExceptionType.unknown) {
+    log('An unexpected error occurred: ${e.message}');
+    return 'An unexpected error occurred. Please try again later.';
+  } else if (e.type case DioExceptionType.cancel) {
+    log('Request was cancelled: ${e.message}');
+    return 'Request was cancelled.';
+  } else {
+    return 'An unexpected error occurred. Please try again later.';
   }
+  return 'An unexpected error occurred. Please try again later.';
 }
