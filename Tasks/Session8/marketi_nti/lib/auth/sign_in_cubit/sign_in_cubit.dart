abdf33a3739385878d6713f606776585f0d21baf
Rober Maher <robermaher@gmail.com>
2026-06-18 21:27:47 +0300

    Refactor sign-in logic by removing unused imports and logging, and improving response handling

--- Tasks/Session8/marketi_nti/lib/auth/sign_in_cubit/sign_in_cubit.dart
+++ Tasks/Session8/marketi_nti/lib/auth/sign_in_cubit/sign_in_cubit.dart
@@ -1,10 +1,11 @@
-import 'dart:developer';
-
 import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:marketi_nti/auth/models/sign_in_response_model.dart';
+import 'package:marketi_nti/config/cache/cache_constants.dart';
+import 'package:marketi_nti/config/cache/cache_helper.dart';
 import 'package:marketi_nti/core/networking/api_consumer.dart';
 import 'package:marketi_nti/core/networking/handle_error.dart';
-import 'package:meta/meta.dart';
 
 part 'sign_in_state.dart';
 
@@ -13,14 +14,20 @@ class SignInCubit extends Cubit<SignInState> {
   final ApiConsumer apiConsumer;
 
   Future<void> signIn({required String email, required String password}) async {
-    log("email: $email, password: $password");
-
     try {
       emit(SignInLoading());
-      await apiConsumer.post(
+      Response response = await apiConsumer.post(
         url: 'https://accessories-eshop.runasp.net/api/auth/login',
         data: {"email": email, "password": password},
       );
+
+      SignInResponseModel signInResponseModel = SignInResponseModel.fromJson(response.data);
+
+      CacheHelper.setSecureData(
+        key: CacheConstants.accessToken,
+        value: signInResponseModel.accessToken!,
+      );
+
       emit(SignInSuccess());
     } on DioException catch (e) {
       emit(SignInFailure(errorMessage: handleError(e)));
