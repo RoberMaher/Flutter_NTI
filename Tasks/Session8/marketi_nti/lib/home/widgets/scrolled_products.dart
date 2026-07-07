deee7bccb0e027eb84b6fd52f448ba16b7751afb
Rober Maher <robermaher@gmaio.com>
2026-07-08 01:32:14 +0300

    Refactor ScrolledProducts: update onFavTap and onStarTap to use ProductModel; enhance UI with Skeletonizer for loading state

--- Tasks/Session8/marketi_nti/lib/home/widgets/scrolled_products.dart
+++ Tasks/Session8/marketi_nti/lib/home/widgets/scrolled_products.dart
@@ -1,7 +1,9 @@
+// home/widgets/scrolled_products.dart
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:marketi_nti/home/widgets/product_model.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+import 'package:marketi_nti/home/models/product_model.dart';
 import 'package:marketi_nti/home/widgets/product_card.dart';
 
 class ScrolledProducts extends StatelessWidget {
@@ -13,28 +15,34 @@ class ScrolledProducts extends StatelessWidget {
   });
 
   final List<ProductModel> products;
-  final Function(int index) onFavTap;
-  final Function(int index) onStarTap;
+  final Function(ProductModel product) onFavTap;
+  final Function(ProductModel product) onStarTap;
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      options: CarouselOptions(
-        height: 144.h,
-        viewportFraction: 0.46,
-        enableInfiniteScroll: false,
-        clipBehavior: Clip.none,        
-      ),
-      items: products.asMap().entries.map((entry) {
-        final index = entry.key;
-        final product = entry.value;
+    final isLoading = products.isNotEmpty && products.first.id == 0;
 
-        return ProductCard(
-          product: product,
-          onFavTap: () => onFavTap(index),
-          onStarTap: () => onStarTap(index),
-        );
-      }).toList(),
+    return Skeletonizer(
+      enabled: isLoading,
+      child: CarouselSlider(
+        options: CarouselOptions(
+          height: 144.h,
+          viewportFraction: 0.46,
+          enableInfiniteScroll: false,
+          clipBehavior: Clip.none,
+          enlargeCenterPage: false,
+        ),
+        items: products.map((product) {
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 6.w),
+            child: ProductCard(
+              product: product,
+              onFavTap: isLoading ? () {} : () => onFavTap(product),
+              onStarTap: isLoading ? () {} : () => onStarTap(product),
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
